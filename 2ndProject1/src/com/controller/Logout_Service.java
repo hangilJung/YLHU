32ee4ad184003f060beb57757b35b92e9851314b
SMT016 <SMT016@183.105.60.32>
2020-09-22 23:15:21 +0900

    로그아웃

--- 2ndProject1/src/com/controller/Logout_Service.java
+++ 2ndProject1/src/com/controller/Logout_Service.java
@@ -2,6 +2,7 @@ package com.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.front.Command;
 
@@ -9,8 +10,10 @@ public class Logout_Service implements Command {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) {
-		// TODO Auto-generated method stub
-		return null;
+		HttpSession session = request.getSession();
+		session.removeAttribute("info");
+		// interface 저장을 안했네요 아감사합니다! ㅎ넵!
+		return "main.jsp";
 	}
 
 }

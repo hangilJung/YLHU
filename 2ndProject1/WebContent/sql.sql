8b40b916daa3968006e65de488f99a0780fd033f
SMT042 <SMT042@220.71.175.227>
2020-09-24 16:35:42 +0900

    0924 메인 메뉴바 수정

--- 2ndProject1/WebContent/sql.sql
+++ 2ndProject1/WebContent/sql.sql
@@ -1,73 +1,63 @@
-1. 회원가입한 정보를 저장할 수 있는 'news_member'테이블을 만드시오.
-   컬럼명 : email / pw / age / tel
 
-drop table news_member;
+drop table news_memeber;
+drop table news_message;
+drop table main_service;
+
+
+
+
+
+
+
 
 
 create table news_member(
-	email varchar2(100),
-	pw varchar2(100),
-	age number,
-	tel varchar2(100)
+	            email varchar2(100) constraint news_member_email_pk primary key,
+	            pw varchar2(100),
+	            age number,
+	            tel varchar2(100)
 );
-
-
-
-insert into news_member values('admin','123',20,'010-1234-4321');
-select * from news_member;
-
 
 create table news_message(
-	msg_num number,
-	sender varchar2(100),
-	msg_mail varchar2(100),
-	title varchar2(100),
-	content varchar2(1000),
-	msg_day date
+            msg_num number constraint news_message_msg_num_pk primary key,
+            sender varchar2(100),
+            msg_mail varchar2(100),
+            title varchar2(100),
+            content varchar2(1000),
+            msg_day date
 );
 
-create sequence msg_num start with 1 increment by 1
-
-insert into news_message values(msg_num.nextval,'don','123','title','감투는 어떄..?',sysdate);
-insert into news_message values(msg_num.nextval,'jae','123','title','파도가 끝나는 곳까지...',sysdate);
-
-select * from news_message;
-
-
-
-
-drop table news_message;
-
-
-drop table main_service
-
-
-
 create table main_service(
+            mase_num number,	
+            mase_email varchar2(30),
+            mase_title varchar2(300),
+            mase_content varchar2(3000),
+            mase_source varchar2(300),
+            mase_date date,
+            constraint main_service_mase_num_pk primary key(mase_num)
+);
 
-	mase_num number,	
-	mase_email varchar2(30),
-	mase_title varchar2(300),
-	mase_content varchar2(3000),
-	mase_source varchar2(300),
-	mase_date date
-)
 
-create sequence mase_num start with 1 increment by 1
 
+
+select * from news_member;
+select * from news_message;
 select * from main_service;
 
 
-create table board_comment(
-	comment_num number,
-	comment_board number,
-	comment_email varchar2(30),
-	comment_parent number,
-	comment_content varchar2(1000),
-	comment_date date	
-)
-
-create sequence comment_seq;
 
 
-drop table comment_board
+create sequence msg_num start with 1 increment by 1
+create sequence mase_num start with 1 increment by 1
+
+
+
+
+drop sequence msg_num
+drop sequence mase_num
+
+
+
+
+
+
